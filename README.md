c9da3669708cf087e15032ac72fccf62640002b1
ThinkStationC30Windows10\mazhe <laggardkernel@outlook.com>
2016-10-21 21:51:20 +0800

    Setprop by replacing build.prop on demand.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # ViPER4Android
 Support Link: http://forum.xda-developers.com/showthread.php?t=2191223
 
-Trying to get more compatibility for v4a. Still testing.
+Use tweaks on v4a-Magisk for more compatibility.
+
+1.deep_buffer and raw removal for more compatibility.
+2.Fix broken /system/vendor/etc/audio_effects.conf on OnePlus3Oxygen.
+3.Use "CACHEMOD=true" to setprops by modifying build.prop on demand.
--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -6,12 +6,10 @@
 /data/magisk/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
 /data/magisk/sepolicy-inject --live -s audioserver -t audioserver_tmpfs -c file -p read,write,execute
 
-#setprop ro.audio.samplerate 48000
-#setprop ro.audio.pcm.samplerate 48000
-setprop audio.deep_buffer.media false
+#setprop audio.deep_buffer.media false
 #setprop lpa.decode false
 #setprop tunnel.decode false
 #setprop tunnel.audiovideo.decode false
 #setprop lpa.releaselock false
 #setprop lpa.use-stagefright false
-setprop persist.sys.media.use-awesome 1
+#setprop persist.sys.media.use-awesome 1
